f1a751f79fa9155b2ecaff2872b5febe4b22f55b
Remi Grangeon <rgrangeo@z1r2p4.42lyon.fr>
2025-10-27 18:05:16 +0100

    entrypoint

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,34 +1,36 @@
 #!/bin/sh
 set -e
 
-if [ ! -f "wp-load.php" ]; then
-  curl -sSL https://wordpress.org/latest.tar.gz | tar -xz --strip-components=1
-  chown -R www-data:www-data /var/www/html
+mkdir -p /run/php && chown www-data:www-data /run/php
+
+sed -i 's|^listen = .*|listen = 0.0.0.0:9000|' /etc/php/8.1/fpm/pool.d/www.conf
+
+if [ ! -f /var/www/html/index.php ]; then
+    wp core download --path=/var/www/html --version=6.8 --allow-root
 fi
 
-sed -i 's|^listen = .*|listen = 0.0.0.0:9000|' /etc/php/*/fpm/pool.d/www.conf
+if [ ! -f /var/www/html/wp-config.php ]; then
+    wp config create \
+        --dbname="$DB_NAME" \
+        --dbuser="$DB_USER" \
+        --dbpass="$DB_PASSWORD" \
+        --dbhost="$DB_HOST" \
+        --path=/var/www/html \
+        --allow-root
 
-if [ ! -f "wp-config.php" ]; then
-  wp config create \
-    --path=/var/www/html \
-    --dbname="${DB_NAME}" \
-    --dbuser="${DB_USER}" \
-    --dbpass="${DB_PASSWORD}" \
-    --dbhost="${DB_HOST}" \
-    --skip-check \
-    --allow-root
 fi
 
-if ! wp core is-installed --allow-root; then
-  wp core install \
-    --url="${WP_URL}" \
-    --title="${WP_TITLE}" \
-    --admin_user="${WP_ADMIN_USER}" \
-    --admin_password="${ADMIN_PASS}" \
-    --admin_email="${WP_ADMIN_EMAIL}" \
-    --skip-email \
-    --allow-root
+if ! wp core is-installed --path=/var/www/html --allow-root; then
+    wp core install \
+        --url="$WP_URL" \
+        --title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN_USER" \
+        --admin_password="$WP_ADMIN_PASSWORD" \
+        --admin_email="$WP_ADMIN_EMAIL" \
+        --path=/var/www/html \
+        --allow-root
 fi
+
 
 if wp user get admin1 --path=/var/www/html --allow-root > /dev/null 2>&1; then
     wp user delete admin1 --allow-root --path=/var/www/html --yes
@@ -41,6 +43,11 @@ if ! wp user get "$WP_NORMAL_USER" --path=/var/www/html --allow-root > /dev/null
         --user_pass=$WP_NORMAL_PASSWORD \
         --path=/var/www/html \
         --allow-root
+fi
+
+
+if ! wp theme install astra --path=/var/www/html --allow-root; then
+    wp theme install astra --activate --allow-root
 fi
 
 exec "$@"
