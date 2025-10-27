0c37afeedeab7838466d2917c375c0c7413e1ad6
Remi Grangeon <rgrangeo@z1r2p4.42lyon.fr>
2025-10-27 17:24:29 +0100

    wordpress

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -6,6 +6,8 @@ if [ ! -f "wp-load.php" ]; then
   chown -R www-data:www-data /var/www/html
 fi
 
+sed -i 's|^listen = .*|listen = 0.0.0.0:9000|' /etc/php/8.1/fpm/pool.d/www.conf
+
 if [ ! -f "wp-config.php" ]; then
   wp config create \
     --path=/var/www/html \
@@ -26,6 +28,19 @@ if ! wp core is-installed --allow-root; then
     --admin_email="${WP_ADMIN_EMAIL}" \
     --skip-email \
     --allow-root
+fi
+
+if wp user get admin1 --path=/var/www/html --allow-root > /dev/null 2>&1; then
+    wp user delete admin1 --allow-root --path=/var/www/html --yes
+fi
+
+
+if ! wp user get "$WP_NORMAL_USER" --path=/var/www/html --allow-root > /dev/null 2>&1; then
+    wp user create "$WP_NORMAL_USER" "$WP_NORMAL_EMAIL" \
+        --role=editor \
+        --user_pass=$WP_NORMAL_PASSWORD \
+        --path=/var/www/html \
+        --allow-root
 fi
 
 exec "$@"
