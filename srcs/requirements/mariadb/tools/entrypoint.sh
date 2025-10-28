9f898427ca4859b696c79945796a8a64433f1f4f
rgrangeo <remi.grangeoncha@gmail.com>
2025-10-28 17:13:05 +0100

    please

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,45 +1,44 @@
 #!/bin/bash
 
-# Création des répertoires nécessaires
 mkdir -p /var/lib/mysql /run/mysqld
 chown -R mysql:mysql /var/lib/mysql /run/mysqld
 
-# Initialisation de la base de données si elle n'existe pas
-if [ ! -d "/var/lib/mysql/mysql" ]; then
+if [ ! -f "/var/lib/mysql/.initialized" ]; then
     echo "Initialisation de la base de données..."
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    
+    if [ ! -d "/var/lib/mysql/mysql" ]; then
+        mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    fi
 
-    # Démarrage temporaire de MariaDB pour l'initialisation
-mysqld_safe --datadir=/var/lib/mysql &
-pid="$!"
+    mysqld_safe --datadir=/var/lib/mysql &
+    pid="$!"
 
-sleep 3
+    echo "Attente du démarrage de MariaDB..."
+    sleep 5
 
     mysql << EOF
--- Suppression des utilisateurs par défaut
 DELETE FROM mysql.user WHERE User='';
 
-
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 
 CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
 
-
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
 
-
 FLUSH PRIVILEGES;
 EOF
 
-echo "Arret temporaire..."
-mysqladmin shutdown
-wait "$pid"
+    echo "Stop temp MariaDB..."
+    mysqladmin -uroot -p${MYSQL_ROOT_PASSWORD} shutdown
+    wait "$pid"
+    
+    touch /var/lib/mysql/.initialized
+    echo "Init finish !"
 fi
 
 echo "Démarrage de MariaDB..."
-# Démarrage de MariaDB en mode normal
 mariadbd --user=mysql --datadir=/var/lib/mysql --bind-address=0.0.0.0
